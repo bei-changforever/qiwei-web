e6609b6a574bc66f037c3a1b59772c279eea1ace
FJB5201314 <DJNB13729250440@163.com>
2024-10-09 18:06:03 +0800

    布局

--- src/views/Product/product-info.vue
+++ src/views/Product/product-info.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="product-info">
-    product-info
+    <div class="product-info-container">
+      <div class="product-content">
+        <div class="product-left">
+          <div class="big-image">
+          <img src="" alt="">
+          </div>
+          <div class="small-image-control">
+            <div class="prev"></div>
+            <div class="small-image"></div>
+            <div class="next"></div>
+          </div>
+        </div>
+        <div class="product-right"></div>
+      </div>
+    </div>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getAssetsFile } from '@/utils/tools'
+</script>
 <style scoped lang="scss">
 .product-info {
-    width: 100%;
+  width: 100vw;
+
+  .product-info-container {
+    width: var(--base-width);
+    margin: 0 auto;
+    background-color: pink;
+
+    .product-content {
+      width: 100%;
+    }
+  }
 }
 </style>
