09713638e69bc6fe845d65e59ac50164a8547c9d
FJB5201314 <DJNB13729250440@163.com>
2024-10-12 17:57:47 +0800

    业务合作

---
+++ src/views/Business/business-serve.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="business-serve">
+    <div class="business-serve-base-container">
+      <div class="bussiness-container-top">
+        <div class="title">为顾客提供的服务</div>
+        <div class="desc">电商/微商/商超链锁/私域/跨境/院线/一站式代加工服务</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+.business-serve {
+  width: 100vw;
+  height: 100vh;
+
+  .business-serve-base-container {
+    margin: 0 auto;
+    width: var(--base-width);
+    transition: all 0.3s ease-in;
+    zoom: 1;
+  }
+}
+</style>
